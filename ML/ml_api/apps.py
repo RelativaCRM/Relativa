9178bf3665937bb80e6e2c5ff4a59c39b9b3beb0
Vlad Antoniuk <167782894+totalbyte@users.noreply.github.com>
2026-04-25 11:57:32 +0300

    fix: remove training logic and models from CR-146 ticket

--- ML/ml_api/apps.py
+++ ML/ml_api/apps.py
@@ -1,27 +1,6 @@
-import os
-import joblib
 from django.apps import AppConfig
 
 
 class MlApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ml_api'
-    
-    # Глобальні змінні для зберігання моделей в оперативній пам'яті
-    closure_model = None
-    churn_model = None
-
-    def ready(self):
-        # Цей код виконується 1 раз при старті сервера
-        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        
-        closure_path = os.path.join(base_dir, 'ml_api', 'models', 'closure_model.pkl')
-        churn_path = os.path.join(base_dir, 'ml_api', 'models', 'churn_model.pkl')
-
-        # Завантажуємо моделі, якщо файли існують
-        if os.path.exists(closure_path) and os.path.exists(churn_path):
-            MlApiConfig.closure_model = joblib.load(closure_path)
-            MlApiConfig.churn_model = joblib.load(churn_path)
-            print("ML models loaded successfully!")
-        else:
-            print("ML model files not found.")
